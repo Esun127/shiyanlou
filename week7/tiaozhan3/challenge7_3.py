26bf32b612750a848e515e7a4640b45552bca816
esun127 <esun127>
2018-12-03 11:21:12 +0800

    finsiehd tiaozhan3 in week7

--- week7/tiaozhan3/challenge7_3.py
+++ week7/tiaozhan3/challenge7_3.py
@@ -6,49 +6,60 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def climate_plot():
+    df_temperature = pd.read_excel("GlobalTemperature.xlsx", index_col="Date")
+    df_temperature.index = pd.DatetimeIndex(df_temperature.index.values)
+    avg_temp = df_temperature.loc[:, ["Land Average Temperature","Land And Ocean Average Temperature"]]
 
+    avg_temp1 = avg_temp.resample('A').mean()
+    avg_temp2 = avg_temp.resample('Q').mean()
+    
+    codelist = ['EN.ATM.CO2E.KT','EN.ATM.METH.KT.CE','EN.ATM.NOXE.KT.CE','EN.ATM.GHGO.KT.CE','EN.CLC.GHGR.MT.CE']
 
-df_temperature = pd.read_excel("GlobalTemperature.xlsx", index_col="Date")
-df_temperature.index = pd.DatetimeIndex(df_temperature.index.values)
-avg_temp = df_temperature.loc[:, ["Land Average Temperature","Land And Ocean Average Temperature"]]
+    df_climate = pd.read_excel("ClimateChange.xlsx", sheetname='Data')
+    df_climate = df_climate[df_climate["Series code"].isin(codelist)]  
 
-
-avg_temp = avg_temp.resample('A').mean()
-
-df_climate = pd.read_excel("ClimateChange.xlsx", sheetname='Data')
-df_climate = df_climate[(df_climate["Series code"] == "EN.ATM.CO2E.KT") |\
-                             (df_climate["Series code"] == "EN.ATM.METH.KT.CE") |\
-                              (df_climate["Series code"] == "EN.ATM.NOXE.KT.CE") |\
-                              (df_climate["Series code"] == "EN.ATM.GHGO.KT.CE") |\
-                              (df_climate["Series code"] == "EN.CLC.GHGR.MT.CE")]
-
-
-                              
-df_climate.replace('..', np.nan, inplace=True)
-
-emission_total = df_climate.iloc[:,6:].T
-
-emission_total.index = pd.to_datetime(emission_total.index.values, format="%Y", )
-
-emission_total = emission_total.fillna(method="ffill").fillna(method="bfill").fillna(value=0)    
-emission_total = emission_total.sum(axis=1)
-emission_total = emission_total.resample("A").sum()
-emission_total = emission_total.to_frame(name="Total GHG")
+    df_climate.replace('..', np.nan, inplace=True)
+    emission_total = df_climate.iloc[:,6:].T
+    emission_total.index = pd.to_datetime(emission_total.index.values, format="%Y", )
+    emission_total = emission_total.fillna(method="ffill").fillna(method="bfill").fillna(value=0)    
+    emission_total = emission_total.sum(axis=1)
+    emission_total = emission_total.resample('A').sum()
+    emission_total = emission_total.to_frame(name="Total GHG")
 
 
 
+    data = pd.concat((avg_temp1.loc['1990-12-31':'2010-12-31'], emission_total.loc['1990-12-31':'2010-12-31']), axis=1)
+    data = data.apply(func=lambda x: (x-x.min())/(x.max()-x.min()))
+    data.dropna(axis=0, inplace=True)
+    data.index = data.index.year
+
+    fig, axes = plt.subplots(2,2)
+
+    ax = data.plot.line(ax=axes[0,0])
+    ax.set_xlabel("Years")
+    ax.set_ylabel("Values")
+
+    ax2 = data.plot.bar(ax=axes[0,1])
+    ax2.set_xlabel("Years")
+    ax2.set_ylabel("Values")
 
 
-data = pd.concat((avg_temp, emission_total), axis=1)
-data = data.apply(func=lambda x: (x-x.min())/(x.max()-x.min()))
-data.dropna(axis=0, inplace=True)
+    ax3 = avg_temp2.plot.area(ax=axes[1,0])
+    ax3.set_xlabel("Quarters")
+    ax3.set_ylabel("Temperature")
 
-#print(data)
 
-fig = plt.subplot()
+    ax4 = avg_temp2.plot.kde(ax=axes[1,1])
+    ax4.set_xlabel("Values")
+    ax4.set_ylabel("Values")
 
-data.plot()
-plt.xticks(data.index)
-# # fig.plot(x=data.index, y=data.iloc[:,1])
+    plt.show()
+    return fig
 
-plt.show()
+if __name__ == '__main__':
+    print(climate_plot())
+
+
+
+
